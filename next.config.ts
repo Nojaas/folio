2886ef7e4fd79e97ee55359780cd4ad3965b6418
Nojaas <jasonleroyle91@gmail.com>
2025-10-14 12:39:16 +0200

    Revert "feat: bypass ad blockers pour Vercel Analytics"
    This reverts commit 0c711a1d124687ba6d1751e5dba0c6c72c95819b.

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/stats/:match*",
-        destination: "https://va.vercel-scripts.com/:match*",
-      },
-    ];
-  },
+  /* config options here */
 };
 
 export default nextConfig;
